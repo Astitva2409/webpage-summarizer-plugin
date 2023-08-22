930f8d3264f40d174b83fb7304c02dcbe7924f5d
Astitva Singh <singhastitva99@gmail.com>
2023-08-22 17:00:45 +0530

    updated

--- webpage_summarizer_plugin/__init__.py
+++ webpage_summarizer_plugin/__init__.py
@@ -1,10 +1,18 @@
-import sys
-import os
+# import sys
+# import os
 
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+# sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# from webpage_summarizer_plugin import routes
+
 
 from flask import Flask
 
-app = Flask(__name__)
 
-from webpage_summarizer_plugin import routes
+def create_app():
+    app = Flask(__name__)
+    return app
--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,4 @@ from webpage_summarizer_plugin import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run()
